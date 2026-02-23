0477216eac7edd9d889653eaed55ce1b905abfe6
danjiv <danjiv@gmail.com>
2026-02-23 19:55:08 +0000

    periodic commit

--- testing_distributional_assumptions.R
+++ testing_distributional_assumptions.R
@@ -61,20 +61,34 @@ probs <- pexp(breakpoints, rate=3)
 probs1 <- probs[2:length(probs)]
 probs2 <- probs[1:length(probs)-1]
 probs3 <- probs1 - probs2
+# add on the probability that a val is larger than the final breakpoint
+probs3 <- c(probs3, pexp(breakpoints[length(breakpoints)], rate=3, lower.tail = FALSE))
 # check number of expected values - remember we got rid of the first value
 expected_numbers <- (length(drivers$id)-1) * probs3
 # number of expected values in each bin doesn't look terrible
 #goodness of fit test should have 23 degrees of freedom, given there 24 values
-# the test stat value looks ridiculous: T = 719.622, which is obviously massively significant
+# the test stat value looks ridiculous: T = 740.931, which is obviously massively significant
 # So obviously we would reject BoxCar's assumption that the interarrival time is 3/hour
-test1 <- sum((p2$counts-expected_numbers)**2 / expected_numbers) 
+test1 <- sum((c(p2$counts, 0)-expected_numbers)**2 / expected_numbers) 
 #but then if we look at the MLE for the sample data, we get back 4718/sum(interarrival_times) = 4.742254
 # Looking at the diff between counts and expected numbers, the real damage is being done in the first bin
 # so repeating the analysis removing the large, tail outliers would have no real impact on the test stat.
 # repeating the test to use Kolmogorov-Smirnov:
 test2 <- ks.test(interarrival_times, "pexp", rate=3, alternative = "two.sided")
 # again the p-value is massively significant!
-
+#
+# testing that the data follow a exponential(4.742253) distribution
+probs_new <- pexp(breakpoints, rate=4.742253)
+#want the probability of a value falling within each bin
+probs1_new <- probs_new[2:length(probs_new)]
+probs2_new <- probs_new[1:length(probs_new)-1]
+probs3_new <- probs1_new - probs2_new
+probs3_new <- c(probs3_new, pexp(breakpoints[length(breakpoints)], rate=4.742253, lower.tail = FALSE))
+expected_numbers_new <- (length(drivers$id)-1) * probs3_new
+test1_new <- sum((c(p2$counts, 0)-expected_numbers_new)**2 / expected_numbers_new)
+#test stat should follow a chi-squared distribution with 22 degrees of freedom
+pchisq(test1_new, 22, lower.tail=FALSE)
+# test looks non significant - all good!
 
 
 #2. Once a driver becomes available, the driver stays available for a random time which is uniformly
@@ -89,6 +103,20 @@ max(driver_working_time)
 # have a look
 d1 <- hist(driver_working_time, breaks = "FD")
 # that looks pretty solidly uniformly distributed between 6 and 8 hours
+# testing...
+breakpoints2 <- d1$breaks
+probs_drivers <- punif(breakpoints2, min = 6, max = 8)
+#want the probability of a value falling within each bin
+probs1_drivers <- probs_drivers[2:length(probs_drivers)]
+probs2_drivers <- probs_drivers[1:length(probs_drivers)-1]
+probs3_drivers <- probs1_drivers - probs2_drivers
+expected_numbers_drivers <- length(drivers$id) * probs3_drivers
+# test should follow a chi-squared distribution with 18 degrees of freedom
+test_drivers <- sum(((d1$counts - expected_numbers_drivers)**2) / expected_numbers_drivers)
+pchisq(test_drivers, 18, lower.tail=FALSE)
+# non-significant, assumption of uniform(6, 8) looks good
+
+
 
 
 #3. The location where the driver becomes available is equally likely to be anywhere in Squareshire, i.e. follows
@@ -112,6 +140,32 @@ full_grid_observed <- ifelse(full_grid %in% names(initial_grid_position),
 full_grid_expected <- rep(length(drivers$id) * 1/(grid_side)**2, grid_side**2)
 test4 <- sum(((full_grid_observed - full_grid_expected)**2 / full_grid_expected))
 # Again, looks massively significant, so clearly initial position is not uniformly distributed over Squareshire
+# looking at the x and y coords
+dx1 <- hist(drivers_x_coords, breaks = "FD")
+# that looks normally distributed
+#
+dy1 <- hist(drivers_y_coords, breaks = "FD")
+# also looks normally distributed
+#
+# looking at the covariance
+dxy_cov <- cov(drivers_x_coords, drivers_y_coords)
+# testing correlation
+dxy_cor <- cor.test(drivers_x_coords, drivers_y_coords,  method = "pearson")
+# don't look at all correlated, can then assume the x-coords and y-coords are sampled from their own
+# normal distributions - will test parameters below.
+xcord_test <- drivers_x_coords[1:2359]
+ycord_test <- drivers_y_coords[1:2359]
+xcord_test_mean <- mean(xcord_test)
+xcord_test_var <- var(xcord_test)
+ycord_test_mean <- mean(ycord_test)
+ycord_test_var <- var(ycord_test)
+# assuming the xcords are normal(10, 19)
+# assuming the ycords are normal(11.5, 19)
+test_drivers_x_pos <- ks.test(drivers_x_coords[2360:4719], "pnorm", mean = 10, sd = sqrt(19), alternative = "two.sided")
+test_drivers_y_pos <- ks.test(drivers_y_coords[2360:4719], "pnorm", mean = 11.5, sd = sqrt(19), alternative = "two.sided")
+# Both non-significant - can go with those distributions
+
+
 
 #RIDERS
 
@@ -125,6 +179,8 @@ r_interarrival_times <- riders$interarrival_times[2:length(riders$interarrival_t
 test3 <- ks.test(r_interarrival_times, "pexp", rate=30, alternative = "two.sided")
 # again p value is massively significant,
 # looking at the MLE for the sample data gives 34420/(sum(r_interarrival_times)) = 34.59611
+# 
+# testing that the data are exponential(34.59611)
 
 
 #2. The origin (the point where the rider appears to demand the ride) and the destination of the trip are independent of
